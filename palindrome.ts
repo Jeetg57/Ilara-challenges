be69d7d6d665aa140e084db78eeac983f95f181f
Jeet <jeetg57@gmail.com>
2021-12-10 18:02:54 +0300

    Palindrome file changed

--- palindrome.ts
+++ palindrome.ts
@@ -3,7 +3,7 @@ function palindrome(str: string) {
   // then reverses the array and joins it into a string
   let reversed = str.split("").reverse().join("");
   //checks to see if the reversed string is equal to input
-  return reversed === str ? true : false;
+  return reversed === str;
 }
 // Run function
 const isPalindrome = palindrome("radar");
